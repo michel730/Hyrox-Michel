8ce8f21df2f100f26405f9d45068b1dc314c5c17
michel730 <michel@legall.cc>
2026-06-18 08:06:16 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE='hyrox-michel-v4';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
+self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)))});
